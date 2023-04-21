1f7d2ce2886bdced3cd71752b9d2bb6b24749c89
Thuong Hoang <ngocanh654189@gmail.com>
2023-04-21 19:05:00 +0700

    Add project files.

---
+++ fap-crawler/Config.cs
@@ -0,0 +1,11 @@
+﻿namespace fap_crawler
+{
+    public class Config
+    {
+        public string? ExecutablePath { get; set; }
+        //public string? HistoryPath { get; set; }
+        //public string? LogPath { get; set; }
+        public string? ResultPath { get; set; }
+        public string? SeedSeparator { get; set; }
+    }
+}
